d730897454bc43afe266aa31a1d44ac8306e22f3
0magical <marilynventura@outlook.com>
2022-10-20 15:04:00 -0400

    added structure to index.js

--- index.js
+++ index.js
@@ -1,16 +1,47 @@
-const { default: inquirer } = require("inquirer");
-
-// TODO: Include packages needed for this application
+const { default: inquirer } = require('inquirer');
+const fs = require('fs')
+const generateMarkdown = require("./utils/generateMarkdown")
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        name:"title",
+        type:"input",
+        message:"What title do you want to bestow to this awesome project?",
+    },
+    {
+        name:""   ,
+        type:""   ,
+        message:""    ,
+    },
+    {
+        name: ""  ,
+        type: ""  ,
+        message: ""   ,
+    },
+    {
+        name: ""   ,
+        type: ""   ,
+        message:  ""   ,
+    },
+    {
+        name: ""   ,
+        type: ""   ,
+        message:  ""   ,
+    },
+    {
+        name: ""   ,
+        type: ""   ,
+        message:  ""   ,
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 function init() {
-inquirer.prompt
+inquirer.prompt(questions)
 }
 
 // Function call to initialize app
